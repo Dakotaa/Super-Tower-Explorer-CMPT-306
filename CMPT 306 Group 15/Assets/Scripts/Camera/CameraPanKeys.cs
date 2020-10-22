af774c082a178690f2957353830d6753c16f4302
bpw537 <bpw537@usask.ca>
2020-10-22 14:45:01 -0600

    Added Camera zoom and pans
    The camera is now able to zoom in and out with the mouse scroll wheel. The camera is able to pan with either dragging the mouse on screen or by using the WASD keys.

---
+++ CMPT 306 Group 15/Assets/Scripts/Camera/CameraPanKeys.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraPanKeys : MonoBehaviour
+{
+    public float panSpeed = 5f;
+    private Vector3 lastPosition;
+
+    private void Update()
+    {
+        // For Keyboard panning
+        lastPosition = transform.position;
+        if (Input.GetKey("w"))
+        {
+            lastPosition.y += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            lastPosition.y -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            lastPosition.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            lastPosition.x += panSpeed * Time.deltaTime;
+        }
+
+        transform.position = lastPosition;
+    }
+}
---
+++ CMPT 306 Group 15/Assets/Scripts/Camera/CameraPanMouse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraPanMouse : MonoBehaviour
+{
+    public float mouseSensitivity = 1f;
+    private Vector3 lastPosition;
+
+    private void Update()
+    {
+        // For Mouse Panning
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 delta = Input.mousePosition - lastPosition;
+            transform.Translate(-delta.x * mouseSensitivity, -delta.y * mouseSensitivity, 0);
+            lastPosition = Input.mousePosition;
+        }
+    }
+}
