bfc0fa7e5114a49dce75c5073fab786aaca68467
Deraj <jae608@mail.usask.ca>
2020-10-23 21:41:43 -0600

    M2 almost done
    Working Copy for M2, but need to fix layering

---
+++ CMPT 306 Group 15/Assets/Scripts/PCGSpawning/CellSpawning.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellSpawning : MonoBehaviour
+{
+    public GameObject cell;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cell, transform.position, Quaternion.identity);
+    }
+}
