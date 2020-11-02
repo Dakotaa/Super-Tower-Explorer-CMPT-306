593f50d5760797342e738c4e5f7718687fee6b34
Shea Slade <55173752+Slade27@users.noreply.github.com>
2020-11-01 22:29:08 -0600

    Pathfinding and enemy spawn with new merge from main
    Added basic enemy spawn and pathfinding

---
+++ CMPT 306 Group 15/Assets/WaveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint; //change this
+
+    public float timeBetweenWaves = 5f;
+
+    private float countdown = 2f; //seconds
+
+    private int waveNumber = 1;
+
+    private void Update()
+    {
+        if (countdown <= 0f) //if coundown up spawn enemies
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+
+        }
+        countdown -= Time.deltaTime; //time passed snce last frame(reduce by 1 every second)
+    }
+
+    void SpawnWave()
+    {
+        Debug.Log("WAVE incoming");
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+        waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+
+}
+
