f5f69a26a12db0e5d031a758e97efac6e2cdc2bc
dcm593 <73200348+dcm593@users.noreply.github.com>
2020-11-04 14:57:17 -0600

    Add files via upload
    Incomplete game control script, also changed the healthbar from being a ui placeholder to being somewhat functional and ends game when it reaches 0

---
+++ GameControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    private List<string> enemies = new List<string>();
+
+    public List<string> GetEnemies()
+    {
+        return enemies;
+    }
+
+    public void SetEnemies(List<string> value)
+    {
+        enemies = value;
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Game Over");
+    }
+}
