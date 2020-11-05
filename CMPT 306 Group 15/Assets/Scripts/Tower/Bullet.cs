761094acef080fa6159840406fec2e685bccdf6b
Dakotaa <kotalalach@gmail.com>
2020-11-04 22:05:01 -0600

    bullet/tower changes

--- CMPT 306 Group 15/Assets/Scripts/Tower/Bullet.cs
+++ CMPT 306 Group 15/Assets/Scripts/Tower/Bullet.cs
@@ -21,6 +21,7 @@ public class Bullet : MonoBehaviour {
 		if (collision.collider.tag.Equals("Enemy")) {	// damage/kill enemies
 			Enemy victim = collision.collider.gameObject.GetComponent<Enemy>();
 			victim.Kill();
+			Destroy(gameObject);
 		}
 	}
 
