a6bdcdd177b66979ce5268037e614a4711eaa344
Dakotaa <kotalalach@gmail.com>
2020-11-12 20:02:39 -0600

    tower tracking and new models
    New tower models.
    Towers now track towards their target (needs some work to make sure the barrel is pointing the right way before shooting)

--- CMPT 306 Group 15/Assets/Scripts/Tower/SimpleTower.cs
+++ CMPT 306 Group 15/Assets/Scripts/Tower/SimpleTower.cs
@@ -3,12 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleTower : Tower {
-	// Start is called before the first frame update
 	int level = 1;
+	public int maxLevel = 5;
+	// change in values for each subsequent level
+	public float levelup_searchInterval = -0.1f;
+	public float levelup_searchRange = -1.0f;
+	public float levelup_cooldown = -0.1f;
+	public float levelup_damage = 0.1f;
+	public float levelup_bulletVelocity = 1.0f;
+
     void Start()
     {
 		base.Start();
     }
+
+	public int GetLevel() {
+		return this.level;
+	}
+
+	public int GetMaxLevel() {
+		return this.maxLevel;
+	}
+
+	public void IncreaseLevel() {
+		if (this.level == this.maxLevel) return;
+		this.level++;
+		this.searchInterval += this.levelup_searchInterval;
+		this.searchRange += this.levelup_searchRange;
+		this.cooldown += this.levelup_cooldown;
+		this.damage += this.levelup_damage;
+		this.bulletVelocity += this.levelup_bulletVelocity;
+	}
 
 
 }
--- CMPT 306 Group 15/Assets/Scripts/Tower/Tower.cs
+++ CMPT 306 Group 15/Assets/Scripts/Tower/Tower.cs
@@ -4,23 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : CellTile {
+	// tower base stats
 	public float searchInterval = 1.0f;	// how often the tower will search for enemies
 	public float searchRange = 5.0f;   // how far the tower will search for enemies
 	public float switchRange = 2.5f; // if the current enemy is further than this, search for a closer enemy
-	public float trackingSpeed = 5.0f;
 	public float cooldown = 1.0f;	// shot cooldown, seconds
 	public float damage = 1.0f; // shot damage
 	public float bulletVelocity = 5.0f;
+	public float trackingSpeed = 5.0f;
+
+	// prefabs and objects
 	public Bullet bullet;
 	// public GameController controller;
+	public List<GameObject> enemies;    // list of existing enemies
+
+	// target tracking
+	private Transform barrel;
 	private GameObject target;  // enemy currently being targeted
-	private Vector3 targetLastPos = Vector3.zero; // last known position of current target
-	Quaternion targetTrackRot;  // rotation of tower
-	public List<GameObject> enemies;	// list of existing enemies
+	private Vector3 lookPos;
+	private float angle;
+	private Quaternion qTo;
+
 
 	public virtual void Start() {
 		target = null; // no initial target
 		enemies = new List<GameObject>();
+		barrel = transform.GetChild(0).GetChild(0);
 		StartCoroutine("DoSearch"); // search coroutine starts on creation, loops forever
 		StartCoroutine("DoShoot"); // shoot coroutine starts on creation, loops forever
 		var bounds = GetComponent<SphereCollider>().bounds; //update the graph
@@ -32,12 +41,22 @@ public class Tower : CellTile {
 		AstarPath.active.UpdateGraphs(guo);
 	}
 
+	public virtual void Update() {
+		if (target) {
+			lookPos = target.transform.position - transform.position;
+			angle = (Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg) - 90.0f;
+			qTo = Quaternion.AngleAxis(angle, Vector3.forward);
+			barrel.rotation = Quaternion.Slerp(barrel.rotation, qTo, trackingSpeed * Time.deltaTime);
+		}
+	}
+
 	// shoot at an enemy
-	public virtual void ShootAt(Vector3 targetPos) {
+	public virtual bool ShootAt(Vector3 targetPos) {
 		Vector3 bulletPos = new Vector3(transform.position.x + 0.5f, transform.position.y + 0.5f, 0.0f);
 		Bullet shotBullet = Instantiate(bullet, bulletPos, Quaternion.identity);
 		Vector3 direction = (targetPos - transform.position).normalized;
 		shotBullet.Setup(direction, bulletVelocity);
+		return true;
 	}
 
 	// set the current target enemy
@@ -83,8 +102,12 @@ public class Tower : CellTile {
 	// tower shoot coroutine
 	public virtual IEnumerator DoShoot() {
 		for (;;) { // loop forever on cooldown
-			if (target) {
-				ShootAt(target.transform.position);
+			bool shot = false;
+			while (!shot) {
+				if (target) {
+					shot = ShootAt(target.transform.position);
+				}
+				yield return null;
 			}
 			yield return new WaitForSeconds(cooldown);
 		}
