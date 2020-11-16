8deb0c7fac5eaae2fb657c5d56b817a72c9ad75f
bpw537 <bpw537@usask.ca>
2020-11-16 14:03:30 -0600

    New click to selct towers
    Made it so users now only need to click on towers in the build menu to drag them around with the mouse. Bug happening with tower tiles not showing up.

--- CMPT 306 Group 15/Assets/Scripts/Cell/MouseTowerCreate.cs
+++ CMPT 306 Group 15/Assets/Scripts/Cell/MouseTowerCreate.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-// Class used to move the tower picture in build menu around on screen
-public class MouseTowerCreate : MonoBehaviour, IDragHandler, IEndDragHandler
+// Class used to move the tower picture in build menu around on screen by dragging
+/*public class MouseTowerCreate : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     public bool isTowerDragged = false;
 
@@ -19,5 +19,30 @@ public class MouseTowerCreate : MonoBehaviour, IDragHandler, IEndDragHandler
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.localPosition = new Vector3(27.5f, -27.5f);
+    }
+}*/
+
+// Class used to move the tower picture in build menu around on screen by clicking
+public class MouseTowerCreate : MonoBehaviour, IPointerClickHandler
+{
+    public bool isTowerDragged = false;
+
+    // Moves the tower picture when mouse button clicked
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isTowerDragged = true;
+    }
+
+    // Moves the tower picture when mouse button clicked
+    public void Update()
+    {
+        if (isTowerDragged)
+        {
+            transform.position = Input.mousePosition;
+        }
+        else
+        {
+            transform.localPosition = new Vector3(27.5f, -27.5f);
+        }
     }
 }
