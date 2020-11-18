77bcd7b17e0e3651ebd964c1bb3facf6dacf1cb1
Deraj <jae608@mail.usask.ca>
2020-11-17 23:02:54 -0600

    Almost have it. Spawn points not hooked up to wave generator

--- CMPT 306 Group 15/Assets/Scripts/Enemy/EnemySpawn.cs
+++ CMPT 306 Group 15/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public bool can_spawn = false;
-
     private WaveControl controller;
 
     private void Start()
@@ -15,13 +13,9 @@ public class EnemySpawn : MonoBehaviour
 
     private void Update()
     {
-        if (can_spawn)
+        if (!controller.spawnPoints.Contains(this.transform))
         {
             controller.spawnPoints.Add(this.transform);
-        }
-        else if (!can_spawn && controller.spawnPoints.Contains(this.transform))
-        {
-            controller.spawnPoints.Remove(this.transform);
         }
     }
 
@@ -29,18 +23,8 @@ public class EnemySpawn : MonoBehaviour
     {
         if (other.CompareTag("EnemySpawn"))
         {
-            print("hiiii");
-            Destroy(other);
-            Destroy(this);
-        }
-
-        else if (other.CompareTag("HideTile"))
-        {
-            can_spawn = true;
-        }
-        else
-        {
-            can_spawn = false;
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
