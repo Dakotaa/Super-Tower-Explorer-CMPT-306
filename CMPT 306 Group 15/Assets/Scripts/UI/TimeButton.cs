01124ae321440242fbfad1f2fba340f31230e582
Dakotaa <kotalalach@gmail.com>
2020-12-05 21:25:00 -0600

    Revert "Revert "Merge branch 'Gamespeed' into Towers3""
    This reverts commit 8c2b26cbab748440d269e2c51deab9efb109c642.

---
+++ CMPT 306 Group 15/Assets/Scripts/UI/TimeButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeButton : MonoBehaviour {
+
+	private SpeedControl speedcontrol;
+
+    void Start() {
+		this.speedcontrol = SpeedControl.instance;
+    }
+
+	public void ChangeSpeed(int speed) {
+		speedcontrol.SetSpeed(speed);
+	}
+
+    
+}
