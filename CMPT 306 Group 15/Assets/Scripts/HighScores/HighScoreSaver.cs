2c2d32c7434b5e08cdac0acb0a3b61bb2fa3e82b
mak918 <mak918@usask.ca>
2020-12-07 16:29:58 -0600

    Added a high scores saver
    Added HighScores class that saves high scores. Added HighScoreSaver static class to save / load a HighScores object to / from a binary file.

---
+++ CMPT 306 Group 15/Assets/Scripts/HighScores/HighScoreSaver.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.ConstrainedExecution;
+
+public static class HighScoreSaver
+{
+    // Determine how long the high score list will be.
+    public static int scoresToSave = 5;
+
+    private static string path = Application.persistentDataPath + "/highscores" + scoresToSave.ToString() + ".save";
+
+    /*
+     * Save the scores of a HighScores object within a binary file.
+     */
+    public static void SaveHighScores(HighScores highScores)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        formatter.Serialize(stream, highScores);
+        stream.Close();
+    }
+
+    /*
+     * Load saved highscores from a binary file into a HighScores object.
+     */
+    public static HighScores LoadHighScores()
+    {
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            HighScores highScores = formatter.Deserialize(stream) as HighScores;
+            stream.Close();
+
+            return highScores;
+        }
+        else
+        {
+            int[] scores = new int[scoresToSave];
+            string[] names = new string[scoresToSave];
+            HighScores highScores = new HighScores(scores, names);
+
+            return highScores;
+        }
+    }
+}
