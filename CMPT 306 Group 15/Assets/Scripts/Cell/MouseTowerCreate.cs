0d0094e7dd4ddd2ff0debf32aac615f578b00899
bpw537 <bpw537@usask.ca>
2020-12-06 18:30:39 -0600

    Updated so towers can be made with multiple resources
    If you go to the button image of the tower in the build menu there it will have the MouseTowerCreate script as an component. There you are able to pick the initial costs of the tower. Then in the CellGrid script at the bottom you can change how much the resources increase after each tower placement.

--- CMPT 306 Group 15/Assets/Scripts/Cell/MouseTowerCreate.cs
+++ CMPT 306 Group 15/Assets/Scripts/Cell/MouseTowerCreate.cs
@@ -3,32 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-/*// Class used to move the tower picture in build menu around on screen by dragging
-public class MouseTowerCreate : MonoBehaviour, IDragHandler, IEndDragHandler
-
-{
-    public bool isTowerDragged = false;
-    public int resourceCost = 1;
-
-    // Moves the tower picture when mouse button held down
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position = Input.mousePosition;
-        isTowerDragged = true;
-    }
-
-    // Moves tower picture back to build menu
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        transform.localPosition = new Vector3(27.5f, -27.5f);
-    }
-}*/
-
 // Class used to move the tower picture in build menu around on screen by clicking
 public class MouseTowerCreate : MonoBehaviour, IPointerClickHandler
 {
     public bool isTowerDragged = false;
-    public int resourceCost = 1;
+    public int woodCost;
+    public int stoneCost;
+    public int metalCost;
     private Vector3 resetPosition = new Vector3(27.5f, -27.5f);
 
     // Moves the tower picture when mouse button clicked
