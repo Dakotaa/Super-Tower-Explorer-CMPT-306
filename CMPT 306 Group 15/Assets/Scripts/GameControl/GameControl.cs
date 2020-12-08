68e5d8a1ac4b911b13f522bfb544b325c1c8442e
Dakotaa <kotalalach@gmail.com>
2020-12-07 20:29:06 -0600

    exp level increase now uses exponential function (1.3)

--- CMPT 306 Group 15/Assets/Scripts/GameControl/GameControl.cs
+++ CMPT 306 Group 15/Assets/Scripts/GameControl/GameControl.cs
@@ -205,6 +205,6 @@ public class GameControl : MonoBehaviour {
 	 * Calculates the EXP needed for the next level.
 	 */
 	private float CalcNextLevel() {
-		return this.EXPForNextLevel * 2.0f;
+		return Mathf.Floor(Mathf.Pow(this.EXPForNextLevel, 1.3f));
 	}
 }
